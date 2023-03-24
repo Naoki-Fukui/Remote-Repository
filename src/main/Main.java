a49d2ba657732f33507a93021390758d278c2a11
Naoki Fukui <fn.driveline@gmail.com>
2023-03-24 23:51:50 +0900

    lesson1.32,1.33兼用

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,18 +3,17 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		Person person1 = new Person("鈴木太郎", 20, 1.7, 84);
+		//	この値がコンストラクタに使用される（※引数の順番はコンストラクタ内に記載の順序と合わせる）
+		Person person1 = new Person("鈴木", "太郎", 20, 1.7, 84);
 		
-//		System.out.println(person1.name);
-//	    System.out.println(person1.age);
-//	    System.out.println(person1.height);
-		
+		//	Person.javaのプリントメソッドを使用
 	    person1.print();
 	    
 	    
 	    
-//	    Person person2 = new Person("山田花子", 22, 1.5, 55);
-//	    person2.print();
+	    Person person2 = new Person("山田", "花子", 22, 1.5, 55);
+	    person2.print();
+	    
 	    
 	    Person.printCount();
 	    
--- src/main/Person.java
+++ src/main/Person.java
@@ -2,36 +2,52 @@ package main;
 
 public class Person {
 	// インスタンスフィールドを定義
-	String name;
-	int age;
-	double height;
-	double weight;
+	// アクセス修飾子でpublicをつけないと他のpackageから読み取れない
+	public int age;
+	public double height;
+	public double weight;
 	public static int count = 0;
-	
-	
+	public String firstName;
+	public String lastName;
+
+
 	// コンストラクタを定義しインスタンスフィールドに値をセット
-	Person(String name, int age, double height, double weight){
-		this.name = name;
+	// 引数に記載する順序はインスタンス生成時の初期値と合わせる必要あり
+	Person(String firstName, String lastName, int age, double height, double weight){
+		//	インスタンスフィールドに引数のageを代入。以下同じ
 		this.age = age;
 		this.height = height;
 		this.weight = weight;
-		this.count += 1;
+		Person.count ++;
+		this.firstName = firstName;
+		this.lastName = lastName;
 	}
-	
+
+
+	public String fullName(){
+		// インスタンスフィールドで定義したfirstNameとlastNameを繋げる処理
+		return this.firstName + this.lastName;
+	}
+
+
+	public void print(){
+		//	fullNameメソッドを実行したときに上の処理が行われ、firstName + lastNameの値が返ってくる
+		//	今回の場合、Main.javaに格納されている鈴木太郎と山田花子
+		System.out.println("名前は" + this.fullName() + "です");
+		System.out.println("年は" + this.age + "です");
+	}
+
 	
 	public double bmi(){
+		//	lesson1.32で使用したBMIを求める式。
+		//	値を代入するのではなく、変数として記載することで数値が変わっても対応可能
 		double BMI = weight / (height * height);
 		return BMI;
 	}
-	
-	
-	public void print() {
-		System.out.println("名前は" + this.name + "です");
-		System.out.println("BMIは" + Math.ceil(this.bmi()) + "です");
-		System.out.println("年は" + this.age + "です");
-	}
-	
+
+
 	public static void printCount() {
+		//	表示した人数をカウントするメソッド
 		System.out.println("合計は" + count + "人です");
 	}
 }
